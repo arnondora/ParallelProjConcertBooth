da5a5491a2c0f2e24e815941c3acc6a066f55de8
arnondora <peter_arnon1@hotmail.com>
2016-12-21 02:01:07 +0700

    Update Footer information with developer info

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,4 @@
 <div class="navbar navbar-default navbar-fixed-bottom" style = "background-color:#3F51B5; color:white;">
-    <p class="navbar-text pull-left">Ticket Booth</p>
+    <p class="navbar-text pull-left">Ticket Booth | <a style = "color:white;" href ="https://github.com/arnondora/ParallelProjConcertBooth">Source Code Available on <span class = "fa fa-github"></span></a></p>
+    <p class ="navbar-text pull-right"><span class = "fa fa-code"></span> with <span class = "fa fa-heart"></span> by <a style = "color:white;" href = "https://www.github.com/arnondora">Arnon</a> and Nagorn</p>
 </div>
